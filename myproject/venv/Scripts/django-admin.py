8ddd2e526029cd9277931d2041fcea67eadfd649
AaronCur <c00202505@itcarlow.ie>
2018-02-01 16:43:51 +0000

    Up to part 2 Experimenting with the models API complete

---
+++ myproject/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gameuser\documents\webdevelopment-assignment3\myproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
