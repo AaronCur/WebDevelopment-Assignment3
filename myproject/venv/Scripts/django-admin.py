a6aea51624e594898d754038f3e6aced9efd795c
AaronCur <C00202505@itcarlow.ie>
2018-02-05 20:56:26 +0000

    Up until part 3 navigation done

--- myproject/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\gameuser\documents\webdevelopment-assignment3\myproject\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
