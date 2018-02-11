0dbad2f3931802feb0d55e38039cec41e0b0eb12
AaronCur <C00202505@itcarlow.ie>
2018-02-11 15:40:59 +0000

    Part 4 complete

--- WebdevCA3/project/boards/tests/test_views.py
+++ WebdevCA3/project/boards/tests/test_views.py
@@ -1,11 +1,10 @@
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import home
-from .views import board_topics, new_topic
-from .models import Board, Topic, Post
+from ..views import board_topics, new_topic, home
+from ..models import Board, Topic, Post
 from django.contrib.auth.models import User
-from .forms import NewTopicForm
+from ..forms import NewTopicForm
 
 # Create your tests here.
 class HomeTests(TestCase):
